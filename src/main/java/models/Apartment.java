8a2e421348ec2ae4f830305392e0e4044cea6cbb
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-12 11:40:46 +0300

    create getters and setters

--- src/main/java/models/Apartment.java
+++ src/main/java/models/Apartment.java
@@ -8,4 +8,52 @@ public abstract class Apartment {
     private int numberOfRooms;
     private int numberOfFloors;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public void setNumberOfFloors(int numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+    }
+
 }
