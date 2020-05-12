30acdef893cdd9d259c4c820e03b12c94ddae9d0
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-12 10:20:38 +0300

    user dao interface complete

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,12 +1,21 @@
 package dao;
 
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
 //    CREATE
-
+    void save(User user);
 
 //    READ
+    List<User>getAll();
+    User findById(int id);
 
 //    UPDATE
+    void update(User user);
 
 //    DESTROY
+    void deleteById(int id);
+    void clearAll();
 }
