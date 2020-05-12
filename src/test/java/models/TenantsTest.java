76425c9b564e79a20bddc7294a2cde9ac3c9ef38
rmogusu <rmogusu123@gmail>
2020-05-12 15:57:06 +0200

    Add test to tenant class and make sure it passess.

--- src/test/java/models/TenantsTest.java
+++ src/test/java/models/TenantsTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,19 @@ public class TenantsTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+    @Test
+    public void NewTenantsObjectGetsCorrectlyCreated_true() throws Exception {
+        Tenants tenants  = setupNewTenant();
+        assertEquals(true, tenants  instanceof Tenants  );
+    }
+    @Test
+    public void User_InstantiatesWithName_true() throws Exception {
+        Tenants tenants  = setupNewTenant();
+        assertEquals("Rose", tenants .getName());
+    }
+    //helper methods
+    public Tenants setupNewTenant(){
+        return new Tenants ("Rose","0712345",10,3,1) ;
     }
 }
