1fcab1f5b328a017ea92fa44673f092e8392c9fb
rmogusu <rmogusu123@gmail>
2020-05-12 15:41:31 +0200

    Add test to instantiate with name and make sure it can pass

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,19 @@ public class UserTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+    @Test
+    public void NewUserObjectGetsCorrectlyCreated_true() throws Exception {
+        User  user  = setupNewUser();
+        assertEquals(true, user instanceof User );
+    }
+    @Test
+    public void User_InstantiatesWithName_true() throws Exception {
+        User user  = setupNewUser();
+        assertEquals("Rose", user.getName());
+    }
+    //helper methods
+    public User setupNewUser(){
+        return new User ("Rose","rmogusu123@gmail.com","rmogusu","12345") ;
     }
 }
