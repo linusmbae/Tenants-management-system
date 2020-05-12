8e9c69f3974179505ce48b1f077870cf16afb420
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-12 11:39:36 +0300

    create abstract class apartment

---
+++ src/main/java/models/Apartment.java
@@ -0,0 +1,11 @@
+package models;
+
+public abstract class Apartment {
+    private int id;
+    private String name;
+    private String location;
+    private String type;
+    private int numberOfRooms;
+    private int numberOfFloors;
+
+}
