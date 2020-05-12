9179b13f6b7cd3d2928f327f4808d9b089507f78
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-12 11:50:22 +0300

    OneBedroomApartment complete

--- src/main/java/models/OneBedroomApartment.java
+++ src/main/java/models/OneBedroomApartment.java
@@ -1,4 +1,13 @@
 package models;
 
-public class OneBedroomApartment {
+public class OneBedroomApartment extends Apartment {
+    private final String TYPE="One Bedroom";
+
+    public OneBedroomApartment(String name, String  location, int numberOfRooms, int numberOfFloors) {
+        this.name=name;
+        this.location=location;
+        this.numberOfRooms=numberOfRooms;
+        this.numberOfFloors=numberOfFloors;
+        this.type=TYPE;
+    }
 }
