beec32438c75666c21608390c0313e4f08da94e7
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-12 10:02:57 +0300

    generate constructor

--- src/main/java/models/user.java
+++ src/main/java/models/user.java
@@ -7,5 +7,10 @@ public class user {
     private String userName;
     private String password;
 
-
+    public user(String name, String email, String userName, String password) {
+        this.name = name;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+    }
 }
