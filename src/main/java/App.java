e89e70fa95b971fb715e92082aa8adc5160341ed
Blackscure <Wekesabuyahi@gmail.com>
2020-05-13 12:05:09 +0300

    main method created in app file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,12 @@
 public class App {
+    public  static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+    }
 }
